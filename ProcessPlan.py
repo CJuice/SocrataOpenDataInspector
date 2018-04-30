3ee534e21034dde361515f5abc0aa1435714658a
CJuice <CJuice@users.noreply.github.com>
2018-04-30 13:07:02 -0400

    Documenting code

--- ProcessPlan.py
+++ ProcessPlan.py
@@ -1,10 +1,9 @@
 """
-PENDING:
+PENDING FUNCTIONALITY
 only processes datasets that have been processed more recently than the last run
     reads the last date processed information from the api metadata for the dataset
+compare previous results against current to see change in the datasets
 """
-# TODO: only process datasets processed since last run of this script (assuming this is regularly scheduled) ??
-# TODO: compare the results of a round of evaluation against previous rounds to see change in the datasets ??
 
 # IMPORTS
 from collections import namedtuple
@@ -42,16 +41,38 @@ assert os.path.exists(CORRECTIONAL_ENTERPRISES_EMPLOYEES_JSON_FILE.value)
 
 # FUNCTIONS (alphabetic)
 def build_csv_file_name_with_date(today_date_string, filename):
-    """"""
+    """
+    Build a string, ending in .csv, that contains todays date and the input file name
+
+    :param today_date_string: Intended to be the date the file will be created
+    :param filename: Name of the file
+    :return: string that is 'date_filename.csv'
+    """
     return "{}_{}.csv".format(today_date_string, filename)
 
 def build_data_providers_inventory(data_providers_dictionary, cleaned_dataset_name, cleaned_provider_name):
-    """"""
+    """
+    From the data freshness report build an inventory of dataset owners
+
+    :param data_providers_dictionary: Dictionary to be populated with the results and returned
+    :param cleaned_dataset_name: Dataset name with illegal characters removed ahead of time
+    :param cleaned_provider_name: Provider name with illegal characters removed ahead of time
+    :return: Dictionary in format of ['dataset name' : 'provider name']
+    """
     data_providers_dictionary[cleaned_dataset_name] = os.path.basename(cleaned_provider_name)
     return
 
 def build_dataset_url(url_root, api_id, limit_amount, offset, total_count):
-    """"""
+    """
+    Build the url used for each request for data from socrata
+
+    :param url_root: Root socrata url common to all datasets
+    :param api_id: ID specific to dataset of interest
+    :param limit_amount: Upper limit on number of records to be returned in response to request
+    :param offset: If more than one request, offset the range of records requested by this amount
+    :param total_count: Current total number of records evaluated during processing of each individual dataset
+    :return: String url
+    """
     # if the record count exceeds the initial limit then the url must include offset parameter
     if total_count >= LIMIT_MAX_AND_OFFSET.value:
         return "{}{}.json?$limit={}&$offset={}".format(url_root, api_id, limit_amount, offset)
@@ -59,7 +80,12 @@ def build_dataset_url(url_root, api_id, limit_amount, offset, total_count):
         return "{}{}.json?$limit={}".format(url_root, api_id, limit_amount)
 
 def build_datasets_inventory(freshness_report_json_objects):
-    """"""
+    """
+    Process json response code for dataset names and api id's and build a dictionary for use
+
+    :param freshness_report_json_objects: json returned by socrata per our request
+    :return: Dictionary in format of ['dataset name' : 'dataset api id']
+    """
     datasets_dictionary = {}
     for record_obj in freshness_report_json_objects:
         dataset_name = record_obj["dataset_name"]
@@ -68,11 +94,22 @@ def build_datasets_inventory(freshness_report_json_objects):
     return datasets_dictionary
 
 def build_today_date_string():
-    """"""
+    """
+    Build a string representing todays date
+
+    :return: string representing date formatted as Year Month Day
+    """
     return "{:%Y%m%d}".format(date.today())
 
 def calculate_percent_null_for_dataset(null_count_total, total_records_processed, number_of_fields_in_dataset):
-    """"""
+    """
+    Calculate the percent of all possible data values, not rows or columns, that are null
+
+    :param null_count_total: Total number of null values
+    :param total_records_processed: Total number or records processed
+    :param number_of_fields_in_dataset: Total number of columns in the dataset
+    :return: Percent value as a float
+    """
     if number_of_fields_in_dataset is None:
         return 0
     else:
@@ -83,15 +120,30 @@ def calculate_percent_null_for_dataset(null_count_total, total_records_processed
             return (float(null_count_total/total_number_of_values_in_dataset)*100)
 
 def calculate_time_taken(start_time):
-    """"""
+    """
+    Calculat the time difference between now and the value passed as the start time
+
+    :param start_time: Time value representing start of processing
+    :return: Difference value between start time and current time
+    """
     return (time.time() - start_time)
 
 def calculate_total_number_of_empty_values_per_dataset(null_counts_list):
-    """"""
+    """
+    Calculate the total number of null values in a dataset
+
+    :param null_counts_list: List of numeric values representing null counts per column in dataset
+    :return: Integer value representing total
+    """
     return sum(null_counts_list)
 
 def generate_freshness_report_json_objects(dataset_url):
-    """"""
+    """
+    Makes request to socrata url for dataset and processes response into json objects
+
+    :param dataset_url: url to which the request is made
+    :return: json objects in dictionary form
+    """
     json_objects = None
     url = dataset_url
     req = urllib2.Request(url)
@@ -109,7 +161,12 @@ def generate_freshness_report_json_objects(dataset_url):
     return json_objects
 
 def grab_field_names_for_mega_columned_datasets(socrata_json_object):
-    """"""
+    """
+    Generate a dictionary of column names. Specific to very large datasets where field names are suppressed by socrata.
+
+    :param socrata_json_object: response.info() json content from socrata
+    :return: dictionary of hidden and visible field names in dataset
+    """
     column_list = None
     field_names_list_visible = []
     field_names_list_hidden = []
@@ -129,7 +186,13 @@ def grab_field_names_for_mega_columned_datasets(socrata_json_object):
     return fields_dict
 
 def handle_illegal_characters_in_string(string_with_illegals, spaces_allowed=False):
-    """"""
+    """
+    Process string, only allowing alpha and numeric. Spaces can be allowed.
+
+    :param string_with_illegals: Input string to be processed
+    :param spaces_allowed: If True, spaces will be allowed
+    :return: String with illegal characters removed.
+    """
     if spaces_allowed:
         re_string = "[a-zA-Z0-9 ]"
     else:
@@ -142,7 +205,13 @@ def handle_illegal_characters_in_string(string_with_illegals, spaces_allowed=Fal
     return concatenated
 
 def inspect_record_for_null_values(field_null_count_dict, record_dictionary):
-    """"""
+    """
+    Inspect the socrata record for the number of null values
+
+    :param field_null_count_dict: dictionary that counts the nulls for each field in the dataset
+    :param record_dictionary: the data record to be evaluated
+    :return: nothing
+    """
     # In the response from a request to Socrata, only the fields with non-null/empty values appear to be included
     record_dictionary_fields = record_dictionary.keys()
     for field_name in field_null_count_dict.keys():
@@ -152,17 +221,37 @@ def inspect_record_for_null_values(field_null_count_dict, record_dictionary):
     return
 
 def load_json(json_file_contents):
-    """"""
+    """
+    Load .json file contents
+
+    :param json_file_contents: contents of a json file
+    :return: the json file contents as a python dictionary
+    """
     return json.loads(json_file_contents)
 
 def read_json_file(file_path):
-    """"""
+    """
+    Read a .json file and grab all contents.
+
+    :param file_path: Path to the .json file
+    :return: the contents of the .json file
+    """
     with open(file_path, 'r') as file_handler:
         filecontents = file_handler.read()
     return filecontents
 
 def write_dataset_results_to_csv(dataset_name, root_file_destination_location, filename, dataset_inspection_results, total_records, processing_time):
-    """"""
+    """
+    Write a csv file containing the analysis results specific to a single dataset
+
+    :param dataset_name: Name of the dataset of interest
+    :param root_file_destination_location: Path to the location where the file directory where the file will be created
+    :param filename: Name of the file, specific to each dataset
+    :param dataset_inspection_results: Results of the data set inspection for null values
+    :param total_records: Total number of records in the dataset
+    :param processing_time: Time it took to process the dataset
+    :return: None
+    """
     file_path = os.path.join(root_file_destination_location, filename)
     if os.path.exists(root_file_destination_location):
         with open(file_path, 'w') as file_handler:
@@ -181,7 +270,19 @@ def write_dataset_results_to_csv(dataset_name, root_file_destination_location, f
     return
 
 def write_overview_stats_to_csv(root_file_destination_location, filename, dataset_name, dataset_csv_file_name, total_number_of_dataset_columns, total_number_of_dataset_records, data_provider, total_number_of_null_fields=0, percent_null=0):
-    """"""
+    """
+    Write analysis results for entire process, as an overview of all datasets, to .csv
+    :param root_file_destination_location: Path to the location where the file directory where the file will be created
+    :param filename: Name of the overview analysis file
+    :param dataset_name: Name of the dataset of interest
+    :param dataset_csv_file_name: Filename of the dataset of interest
+    :param total_number_of_dataset_columns: Total number of columns in dataset of interest
+    :param total_number_of_dataset_records: Total number of records in dataset of interest
+    :param data_provider: Data provider for dataset of interest
+    :param total_number_of_null_fields: Total number of null fields for dataset of interest
+    :param percent_null: The percent null for the dataset of interest
+    :return: None
+    """
     file_path = os.path.join(root_file_destination_location, filename)
     if os.path.exists(root_file_destination_location):
         if not os.path.exists(file_path):
@@ -203,7 +304,15 @@ def write_overview_stats_to_csv(root_file_destination_location, filename, datase
     return
 
 def write_problematic_datasets_to_csv(root_file_destination_location, filename, dataset_name, message, resource=None):
-    """"""
+    """
+    Write to .csv any datasets that encountered problems during processing.
+    :param root_file_destination_location:  Path to the location where the file directory where the file will be created
+    :param filename: Name of the problem datasets file
+    :param dataset_name: Name of the dataset of interest
+    :param message: Message related to reason was problematic
+    :param resource: The url resource that was being processed when problem occurred
+    :return: None
+    """
     file_path = os.path.join(root_file_destination_location, filename)
     if os.path.exists(root_file_destination_location):
         if not os.path.exists(file_path):
@@ -218,7 +327,19 @@ def write_problematic_datasets_to_csv(root_file_destination_location, filename, 
     return
 
 def write_script_performance_summary(root_file_destination_location, filename, start_time, number_of_datasets_in_data_freshness_report, dataset_counter, valid_nulls_dataset_counter, valid_no_null_dataset_counter, problem_dataset_counter):
-    """"""
+    """
+    Write a summary file that details the performance of this script during processing
+
+    :param root_file_destination_location: Path to the location where the file directory where the file will be created
+    :param filename: Name of the performance summary file
+    :param start_time: Time the process started
+    :param number_of_datasets_in_data_freshness_report: Number of datasets in the freshness report
+    :param dataset_counter: The count incremented during processing to count the number of datasets processed
+    :param valid_nulls_dataset_counter: Number of datasets with at least on valid null
+    :param valid_no_null_dataset_counter: Number of datasets with zero detected null values
+    :param problem_dataset_counter: Number of datasets with problems
+    :return: None
+    """
     file_path = os.path.join(root_file_destination_location, filename)
     with open(file_path, 'w') as scriptperformancesummaryhandler:
         scriptperformancesummaryhandler.write("Date,{}\n".format(build_today_date_string()))
@@ -229,10 +350,10 @@ def write_script_performance_summary(root_file_destination_location, filename, s
         scriptperformancesummaryhandler.write("Problematic datasets count,{}\n".format(problem_dataset_counter))
         time_took = time.time() - start_time
         scriptperformancesummaryhandler.write("Process time (minutes),{:6.2f}\n".format(time_took/60.0))
+    return
 
 # FUNCTIONALITY
 def main():
-    """"""
 
     # Initiate csv report files
     problem_datasets_csv_filename = build_csv_file_name_with_date(
